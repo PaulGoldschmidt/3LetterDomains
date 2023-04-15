6067567964c470f8716f23b811b40e5d8ba9db3f
Paul Goldschmidt <github@goldschmidt.id>
2023-04-15 19:31:04 +0200

    Added .com-threeletterdomains

--- generatelist_3letterdomains.py
+++ generatelist_3letterdomains.py
@@ -1,14 +1,14 @@
 import string
 
 # Generate all possible three-letter combinations of letters and digits without a hyphen
-combinations = [a + b + c + '.de' for a in string.ascii_lowercase + string.digits for b in string.ascii_lowercase + string.digits for c in string.ascii_lowercase + string.digits]
+combinations = [a + b + c + '.com' for a in string.ascii_lowercase + string.digits for b in string.ascii_lowercase + string.digits for c in string.ascii_lowercase + string.digits]
 
 # Generate all possible combinations of one letter or digit, a hyphen, and one letter or digit, with the hyphen in the middle
-combinations_with_hyphen = [a + '-' + b + '.de' for a in string.ascii_lowercase + string.digits for b in string.ascii_lowercase + string.digits]
+combinations_with_hyphen = [a + '-' + b + '.com' for a in string.ascii_lowercase + string.digits for b in string.ascii_lowercase + string.digits]
 
 # Combine the two lists of combinations
 combinations += combinations_with_hyphen
 
 # Write the combinations to a file
-with open('threeletterdomains.txt', 'w') as file:
+with open('com_threeletterdomains.txt', 'w') as file:
     file.write('\n'.join(combinations))
