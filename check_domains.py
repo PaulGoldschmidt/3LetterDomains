1c7755dea0eaf665ab51cd928da9605461d9093a
Paul Goldschmidt <github@goldschmidt.id>
2023-04-16 09:39:21 +0200

    Logs now in subdirectory.

--- check_domains.py
+++ check_domains.py
@@ -32,12 +32,15 @@ def send_telegram_message(token, chat_id, message):
 
 
 def rotate_logs():
-    log_files = sorted(glob.glob('log_*.log'))
+    log_files = sorted(glob.glob(os.path.join('logs', 'log_*.log')))
     while len(log_files) > 30:
         os.remove(log_files.pop(0))
 
 
 def main(filepaths, print_to_console=True):
+    if not os.path.exists('logs'):
+        os.makedirs('logs')
+
     rotate_logs()
 
     with open('credentials.json', 'r') as f:
@@ -52,7 +55,7 @@ def main(filepaths, print_to_console=True):
             domains += [line.strip() for line in f.readlines()]
 
     timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
-    log_file = open(f'log_{timestamp}.log', 'w')
+    log_file = open(os.path.join('logs', f'log_{timestamp}.log'), 'w')
     unclaimed_file = open('unclaimed_domains.txt', 'w')
 
     start_time = time.time()
@@ -68,13 +71,12 @@ def main(filepaths, print_to_console=True):
     for i, domain in enumerate(domains):
         if print_to_console:
             print(f'Checking domain {i + 1}/{len(domains)}: {domain}')
-        dns_checked_domains += 1
 
         if is_domain_available(domain) and domain not in previous_unclaimed_domains:
             try:
-                whois_checked_domains += 1
-                time.sleep(1)  # Rate limit: 60 requests per minute
                 domain_info = whois.whois(domain)
+                whois_checked_domains += 1
+                time.sleep(6)  # Rate limit: 10 requests per minute
 
                 if domain_info.status is None:
                     unclaimed_domains.append(domain)
@@ -85,6 +87,8 @@ def main(filepaths, print_to_console=True):
                 log_file.write(f'Error checking domain (WHOIS): {domain} - {str(e)}\n')
                 unclaimed_domains.append(domain)
                 unclaimed_file.write(f'{domain}\n')
+        else:
+            dns_checked_domains += 1
 
         elapsed_time = time.time() - start_time
         estimated_total_time = (elapsed_time / (i + 1)) * len(domains)
@@ -98,33 +102,37 @@ def main(filepaths, print_to_console=True):
     execution_time = end_time - start_time
     median_domains_per_minute = len(domains) / (execution_time / 60)
 
-    log_file.write(f'Start time: {datetime.fromtimestamp(start_time).strftime("%Y-%m-%d %H:%M:%S")}\n')
-    log_file.write(f'End time: {datetime.fromtimestamp(end_time).strftime("%Y-%m-%d %H:%M:%S")}\n')
+    log_file.write(f'Start time: {datetime.fromtimestamp(start_time)}\n')
+    log_file.write(f'End time: {datetime.fromtimestamp(end_time)}\n')
+    log_file.write(f'Total tried domains: {len(domains)}\n')
+    log_file.write(f'DNS checked domains: {dns_checked_domains}\n')
+    log_file.write(f'WHOIS checked domains: {whois_checked_domains}\n')
     log_file.write(f'Execution time: {execution_time:.2f}s\n')
-    log_file.write(f'Domains tried (DNS): {dns_checked_domains}\n')
-    log_file.write(f'Domains tried (WHOIS): {whois_checked_domains}\n')
     log_file.write(f'Median domains checked per minute: {median_domains_per_minute:.2f}\n')
-
-    log_file.write(f'Python version: {platform.python_version()}\n')
-    log_file.write(f'System: {platform.system()} {platform.release()} {platform.version()}\n')
-    log_file.write(f'Machine: {platform.machine()} {platform.processor()}\n')
+    log_file.write(f'Machine info: {platform.uname()}\n')
+    log_file.write(f'Script version: 1.0.0\n')
 
     log_file.close()
     unclaimed_file.close()
 
-    if previous_unclaimed_domains and unclaimed_domains:
-        new_unclaimed_domains = list(set(unclaimed_domains) - set(previous_unclaimed_domains))
+    new_unclaimed_domains = list(set(unclaimed_domains) - set(previous_unclaimed_domains))
+    if new_unclaimed_domains:
+        message = f'New unclaimed domains:\n\n'
+        message += '\n'.join(new_unclaimed_domains)
+        send_telegram_message(telegram_token, telegram_chat_id, message)
 
-        if new_unclaimed_domains:
-            message = 'New unclaimed domains found:\n\n' + '\n'.join(new_unclaimed_domains)
-            send_telegram_message(telegram_token, telegram_chat_id, message)
+    if print_to_console:
+        print(f'\nFinished checking {len(domains)} domains.')
+        print(f'DNS checked domains: {dns_checked_domains}')
+        print(f'WHOIS checked domains: {whois_checked_domains}')
+        print(f'Execution time: {execution_time:.2f}s')
+        print(f'Median domains checked per minute: {median_domains_per_minute:.2f}')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('files', nargs='*', help='File(s) containing the list of domains to check')
-    parser.add_argument('--quiet', action='store_true', help='Disable print to console')
-
+    parser.add_argument('files', nargs='*', help='List of files containing domain names to check')
+    parser.add_argument('--quiet', action='store_true', help='Disable console output')
     args = parser.parse_args()
 
     if not args.files:
